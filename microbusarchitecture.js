c8c62e7bbccc02726f51d90906f1fb706f0af43c
TaiTheThai <tai@taithethai.com>
2017-10-23 14:20:35 -0700

    works with inputfile piped in

--- microbusarchitecture.js
+++ microbusarchitecture.js
@@ -6,5 +6,35 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (input) => {
-  console.log(input);
+  doThis(input);
 });
+
+function doThis(x) {
+  const order = (x.split(/\r?\n|\r/)
+    .map(str => str.slice(0, str.indexOf('#') === -1 ? str.length: str.indexOf('#')).trim())
+    .filter(item => item !== '')
+    .map(num => Number(`0b${num}`)));
+  let register = [0, 0, 0, 0];
+  let active;
+  while (order.length) {
+    switch (order.shift()) {
+      case 1: // clear register
+        register = [0, 0, 0, 0];
+        break;
+      case 2: // set active
+        active = order.shift();
+        break;
+      case 4: // save to active
+        register[active] = order.shift();
+        break;
+      case 5:
+        register[active] = register[order.shift()] * register[order.shift()];
+        break;
+      case 6:
+        console.log(register[active]);
+        break;
+      default:
+        console.log('oops');
+    }
+  }
+}
