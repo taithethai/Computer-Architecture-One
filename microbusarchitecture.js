03f5f5796024c3721e380c715988706751344062
taithethai <taithethai@users.noreply.github.com>
2017-10-23 17:30:11 -0400

    Update microbusarchitecture.js

--- microbusarchitecture.js
+++ microbusarchitecture.js
@@ -38,3 +38,25 @@ function doThis(x) {
     }
   }
 }
+/* Thomson hints:
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (text) {
+  if (text === 'quit\n') {
+    done();
+  }
+  if(text.indexOf('\n')) {
+    const lines = text.split('\n');
+    lines.forEach((line) => {
+      // prep variable
+      const inputBinary = line.split('#')[0].trim();
+      const inputDecimal = Number('0b' + inputBinary);
+      //console.log('decimal' + inputDecimal);
+      //console.log('binary' + inputBinary);
+      if(!isNaN(inputDecimal)) {
+        cpu.process(inputDecimal);
+      }
+    });
+  }  
+});
+*/
